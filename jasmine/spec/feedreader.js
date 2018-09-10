55e97e35b61af9bff0f58a9833ebad53336b202e
Renisha Christie. A <christie.reni@gmail.com>
2018-09-10 20:10:06 +0530

    Write spec for url defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,10 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL should be defined', function() {
+            var urlsNotDefined = allFeeds.filter(feed => feed.hasOwnProperty('url') === false);
+            expect(urlsNotDefined.length).toEqual(0);
+        });
 
 
         /* TODO: Write a test that loops through each feed
